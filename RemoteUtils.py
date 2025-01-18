cfddb2bb295841362bb47e4230e4b6b1bdf8e8e7
njfredri <njfredri@uark.edu>
2025-01-17 21:45:32 -0600

    modified to use password file instead of password. This should be more secure as the actual password is never stored in Python nor printed out in the bash history

--- RemoteUtils.py
+++ RemoteUtils.py
@@ -2,15 +2,15 @@ import subprocess
 import os
 
 class RemoteUtils:
-    def run_cmd(cmd_str, username, remote_host, remote_password:str = None, remotedir:str = None):
+    def run_cmd(cmd_str, username, remote_host, passwordFile:str = None, remotedir:str = None):
         """
         runs command string in a remote bash shell
         """
         print("Command: %s" %(cmd_str))
         
         ssh_call = 'ssh ' + username + '@' + remote_host
-        if remote_password != None:
-            sshpass = 'sshpass -p' + "'" + remote_password + "'"
+        if passwordFile != None:
+            sshpass = 'sshpass -f' + "'" + passwordFile + "'"
             ssh_call = sshpass + ' ' + ssh_call
         
         command = cmd_str
@@ -23,15 +23,15 @@ class RemoteUtils:
         print(sshcommand)
         subprocess.call(sshcommand,shell=True,executable="/bin/bash")
     
-    def run_cmds(cmds: list, username, remote_host, remote_password:str = None, remotedir:str = None):
+    def run_cmds(cmds: list, username, remote_host, passwordFile:str = None, remotedir:str = None):
         """
         runs command string in a remote bash shell
         """
         print("Command: %s" %(str(cmds)))
         
         ssh_call = 'ssh ' + username + '@' + remote_host
-        if remote_password != None:
-            sshpass = 'sshpass -p' + "'" + remote_password + "'"
+        if passwordFile != None:
+            sshpass = 'sshpass -f' + "'" + passwordFile + "'"
             ssh_call = sshpass + ' ' + ssh_call
         
         command = ''
@@ -50,32 +50,32 @@ class RemoteUtils:
     
     # def run_commands
 
-    def copyFileToServer(file, remotedir, username, remote_host, remote_password:str = None):
+    def copyFileToServer(file, remotedir, username, remote_host, passwordFile = None):
         command = 'scp ' + file + ' ' + username + '@' + remote_host + ':' + remotedir
-        if remote_password != None:
-            command = 'sshpass -p ' + remote_host + ' ' + command
+        if passwordFile != None:
+            command = 'sshpass -f ' + passwordFile + ' ' + command
         subprocess.call(command,shell=True,executable="/bin/bash")
 
-    def copyFileFromServer(filename, remotedir, localdir, username, remote_host, remote_password:str = None):
+    def copyFileFromServer(filename, remotedir, localdir, username, remote_host, passwordFile:str = None):
         remotepath = os.path.join(remotedir, filename)
         command = 'scp ' + username + '@' + remote_host + ':' + remotepath + ' ' + localdir
-        if remote_password != None:
-            command = 'sshpass -p ' + remote_host + ' ' + command
+        if passwordFile != None:
+            command = 'sshpass -f ' + passwordFile + ' ' + command
         subprocess.call(command,shell=True,executable="/bin/bash")
         
-    def copyDirToServer(dir, remotedir, username, remote_host, remote_password:str = None):
+    def copyDirToServer(dir, remotedir, username, remote_host, passwordFile:str = None):
         command = 'scp ' + dir + '/* ' + username + '@' + remote_host + ':' + remotedir
-        if remote_password != None:
-            command = 'sshpass -p ' + remote_host + ' ' + command
+        if passwordFile != None:
+            command = 'sshpass -f ' + passwordFile + ' ' + command
 
         print(command)
         subprocess.call(command,shell=True,executable="/bin/bash")
 
         
-    def copyDirFromServer(remotedir, localdir, username, remote_host, remote_password:str = None):
+    def copyDirFromServer(remotedir, localdir, username, remote_host, passwordFile:str = None):
         command = 'scp ' + username + '@' + remote_host + ':' + remotedir + '/* ' + localdir
-        if remote_password != None:
-            command = 'sshpass -p ' + remote_host + ' ' + command
+        if passwordFile != None:
+            command = 'sshpass -f ' + passwordFile + ' ' + command
 
         print(command)
         subprocess.call(command,shell=True,executable="/bin/bash")
